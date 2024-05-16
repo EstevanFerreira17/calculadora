c036abf789dc8d69f79bffffd9923fa191da30ea
Estevan Ferreira <160546147+EstevanFerreira17@users.noreply.github.com>
2024-05-16 19:41:20 -0300

    atualizei repetição de sinal

--- scripts/calc.js
+++ scripts/calc.js
@@ -38,60 +38,56 @@ function transformNum(values) {
 }
 
 function operate(vFinalNm, vFinalSy) {
-    console.log(vFinalNm);  console.log(vFinalSy)
-    
     let res = vFinalNm[0]
-
     for (let [i, value] of vFinalSy.entries()) {
         if (value === '+') {
-            res += vFinalNm[i + 1]
+            res += vFinalNm[i+1]
             console.log(res)
         }
         if (value === '-') {
-            res -= vFinalNm[i + 1]
+            res -= vFinalNm[i+1]
         }
-        if (value === '*') {
-            res *= vFinalNm[i + 1]
+        if (value === 'x') {
+            res *= vFinalNm[i+1]
+        }
+        if (value === '/') {
+            res /= vFinalNm[i+1]
         }
     }
     return res
 }
-
-// let content = '10-10*10+10' //   ['8', '7', '4', '+', '9', '-']
-// const vFinal = filter(content)
-// console.log(vFinal)
-// console.log(operate(vFinal[0], vFinal[1]))
-
 
 const display = document.querySelector('#content') //tela
 const numbers = [...document.querySelectorAll('.num')]; //com numeros
 const symbols = [...document.querySelectorAll('.sym')] //com sinais
 const btnImp = [...document.querySelectorAll('.imp')]
 
-console.log(numbers)
-console.log(symbols)
-console.log(btnImp)
-
 numbers.forEach((n) => {
-    n.addEventListener('click', () => display.innerHTML += `${n.value}`)
-})
-
-symbols.forEach((n) => {
     n.addEventListener('click', () => {
         display.innerHTML += `${n.value}`
     })
 })
 
-//btnImp[0]     //PONTO
-//btnImp[1]     //PARÊNTESES
-
-                //DELETE
+symbols.forEach((s) => {
+    s.addEventListener('click', () => { 
+        display.innerHTML += s.value
+        let a = display.innerHTML.split('')
+        let ant = a.length-2
+        if (a[ant]==='+'||a[ant]==='-'||a[ant]==='x'||a[ant]==='%') {
+            display.innerHTML = a.splice(0, a.length-1).join('')
+        }
+    })
+})
+//btnImp[0]//POINT
+//CLEAR
+btnImp[1].addEventListener('click', () => display.innerHTML='')
+//DELETE
 btnImp[2].addEventListener('click', () => {
     let aux = display.innerHTML.split('')
     let aux2 = aux.splice(0, aux.length-1).join('')
     display.innerHTML = aux2
 })
-
+//EXECUTE (=)
 btnImp[3].addEventListener('click', () => {
     const content = display.innerHTML // Retorno do display
     console.log(content)
@@ -99,7 +95,6 @@ btnImp[3].addEventListener('click', () => {
     console.log(vFinal)
     display.innerHTML = operate(vFinal[0], vFinal[1])
 })
-
 
 
 
